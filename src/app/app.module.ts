6bf8d9a8ce9f529275b42aaf3bf27cbbb3ec8436
Michal Karwowski <michal.karwowski.0@gmail.com>
2020-02-07 13:07:53 +0100

    Update to angular 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { ModelerComponent } from './modeler/modeler.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
